bcaf32b2954e2d6647fb7a70aa39af23420021b9
GNYniamh <gny.niamh@outlook.com>
2022-09-30 10:00:29 -0400

    Create main file
    Create gobot.go file, basic outline to work with as we build our program.  Included comments for ideas and guidelines.

---
+++ golang/gobot.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+
+)
+
+//function compile on regex is a "sinister" func; suppose regex is invalid
+//validateinputregex processes immediately. First thing go executor is doing
+//is figuring out variables, regex.compile doesnt' tell you if the regex
+//fails or not. so MustCompile crashes the server immediately,
+//instead of crashing the server through user's bad
+//input after this point.
+
+//would actually need a redis server on the internet
+//or get a local Ubuntu environment working
+const redisAddress = "localhost:6379"
+
+var validateInputRegex = regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+//this is read as type getHandler equals this next thing
+type getHandler struct {
+	store Client
+}
+
+func main() {
+
+	fmt.Println("Program Starting")
+	//redis.options is a structure initialization - argument to the new client function
+	//& is called referencing or taking the address of or making a pointer from
+	//or getting a pointer to. essentially just pointing it to a thing
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisAddress,
+	})
+
+	// How we know it works / testing
+	key := ""
+	value := ""
+	set(context.Background(), rdb, key, value)
+	/*for testing:
+	output, err := get(rdb, key)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(output)
+	*/
+	//initialize a gethandler struct - giving it a store
+	//handler is an interface that asks you to implement ServeHTTP and we have implemented
+	//that, therefore getHandler is a Handler.
+	getH := getHandler{
+
+		//store on the left - assigning to the field in getHandler
+		//called store. the store on the right is the variable I defined
+		//above appx line 27
+		store: rdb,
+	}
+}
+
+//give a key and a store, try to look up the value
+//can write a precondition: assumption regarding the key (on the inputs)
+//a post condition: the idea is providing that someone meets the precondition, then the post condition makes some guarantees.
+//suppose you had a function that squared a number but suppose you are implementing it yourself and you can only do so on positive numbers
+//so precondition would be number is positive, postcondition is squared number
+
+//io.writestring is a function that takes a generic object and writes to it (ie could take a file or responsewriter or web socket
+//if you were to put in the browser ?get=hello that is a query )
+//ServeHTTP is a function on a getHandler
+func (h getHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	ctx := context.Background()
+	//giving it a name so we don't have to worry about getting it from the URL anymore
+	//when we do things like hit redis we don't want a huge query so a straightaway name assists with the URL and it will be a nice string
+	key := strings.Split(req.URL.Path, "/")[2] //req.URL.Query().Get("key")
+	//log.Println(key)
+	//make sure key is useful now.
+	if key == "" {
+		io.WriteString(w, "You need to give me STUFF\n")
+		return
+	}
+	//now we want to check it actually is a valid key (input)
+	//! means "if not"
+func (h getHandler) handleGetRequest(ctx context.Context, key string, w http.ResponseWriter, req *http.Request) {
+	//now we have a valid key so now we can do get store
+	value, err := get(ctx, h.store, key)
+	//goland assists with coding in go!
+	// type name struct {
+	// 	key   string
+	// 	value string
+	// }
+	// myStruct := name{
+	// 	key:   "hello",
+	// 	value: "hello",
+	// }
+	//I'm gonna use a structure which has a key and a value.
+	//anonymous struct - a struct that you only use in one place
+	jsonbytes, err := json.Marshal(struct {
+		Key   string
+		Value string
+	}{
+		Key:   key,
+		Value: value,
+	})
+	if err != nil {
+		//user sees this message
+		io.WriteString(w, "Error processing.\n")
+		//engineer/person maintaining sees this message
+		log.Println("Error marshalling json.", err.Error())
+		return
+	}
+
+	//will get the query parameter we called "get" and grab the value
+	io.WriteString(w, string(jsonbytes))
+	io.WriteString(w, "\n")
+
+}
+
+//to test this one you can use a curl or use Postman, an HTTP request builder.
+//cant post request from browser
+//so go into ubuntu (or any)terminal
+//$curl -X POST localhost:8080/cache/?value=newValue
+//when you refresh you want to see :newValue in the json
+//in the terminal you could run
+//curl -iv -X POST localhost:8080/cache/?value=newValue
+//http request should reflect
+//developer tools in googlechrome > network tab
+//you can see the error header thingy
